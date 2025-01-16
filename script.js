78b87fdfb8604ec68e530821a77cf7c97fd9cf58
magscop <107082508+magscop@users.noreply.github.com>
2025-01-16 17:25:59 -0500

    udpated

--- script.js
+++ script.js
@@ -199,3 +199,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+
+
+
+document.querySelector('a[href="#interface"]').addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector('#interface');
+    window.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
+    });
+  });
